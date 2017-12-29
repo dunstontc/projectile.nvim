e28896e59c83059c3792c5fbbba8e041d15fb3d4
Clay Dunston <dunstontc@gmail.com>
2017-12-29 02:42:13 -0600

    Cleaned imports & excess funcs in kind/projectile

--- rplugin/python3/denite/kind/projectile.py
+++ rplugin/python3/denite/kind/projectile.py
@@ -3,23 +3,22 @@
 #  FILE: projectile.py
 #  AUTHOR: Clay Dunston <dunstontc@gmail.com>
 #  License: MIT
-#  Last Modified: 2017-12-25
+#  Last Modified: 2017-12-29
 #  =============================================================================
 
-# import re
-import os
 import json
-import datetime
-import subprocess  # TODO: Use denite's proc
+from datetime.datetime import now
+from os.path import basename, isdir, normpath
 
 from ..kind.directory import Kind as Directory
-from denite import util
+from denite.util import expand, input, path2project
 
 
 class Kind(Directory):
     """Kind using JSON to persist data for projects."""
 
     def __init__(self, vim):
+        """Initialize thyself."""
         super().__init__(vim)
         self.name             = 'projectile'
         self.default_action   = 'open'
@@ -34,27 +33,27 @@ class Kind(Directory):
 
     def action_add(self, context):
         """Add a project to ``projectile#data_dir``/projects.json."""
-        data_file = util.expand(self.vars['data_dir'] + '/projects.json')
+        data_file = expand(self.vars['data_dir'] + '/projects.json')
         root_dir  = self.vim.call('getcwd')
         boofer    = self.vim.current.buffer.name
-        pj_root   = util.path2project(self.vim, boofer, '.git')
-        pj_name   = os.path.basename(os.path.normpath(pj_root))
+        pj_root   = path2project(self.vim, boofer, '.git')
+        pj_name   = basename(normpath(pj_root))
         new_data  = {}
 
-        project_root = util.input(self.vim, context, 'Project Root: ', pj_root)
+        project_root = input(self.vim, context, 'Project Root: ', pj_root)
         if not len(project_root):
             project_root = pj_root
 
-        project_name = util.input(self.vim, context, 'Project Name: ', pj_name)
+        project_name = input(self.vim, context, 'Project Name: ', pj_name)
         if not len(project_name):
             project_name = pj_name
 
         new_data = {
             'name':        project_name,
             'root':        project_root,
-            'timestamp':   str(datetime.datetime.now().isoformat()),
+            'timestamp':   str(now().isoformat()),
             'description': '',
-            'vcs':         os.path.isdir(f"{root_dir}/.git")  # TODO: Also check for .hg/ and .svn
+            'vcs':         isdir(f"{root_dir}/.git")  # TODO: Also check for .hg/ and .svn
         }
 
         with open(data_file, 'r') as g:
@@ -72,7 +71,7 @@ class Kind(Directory):
         target       = context['targets'][0]
         target_date  = target['timestamp']
         target_name  = target['name']
-        data_file    = util.expand(self.vars['data_dir'] + '/projects.json')
+        data_file    = expand(self.vars['data_dir'] + '/projects.json')
         confirmation = self.vim.call('confirm', f"Remove {target_name}?", "&Yes\n&No")
         if confirmation == 2:
             return
@@ -89,81 +88,12 @@ class Kind(Directory):
                     json.dump(projects, f, indent=2)
 
     def action_custom(self, context):
-        """Custom action defined by ``g:projectile#directory_command``.
-
-        Parameters
-        ----------
-        context : Object
-
-        """
+        """Execute a custom action defined by ``g:projectile#directory_command``."""
         target   = context['targets'][0]
         user_cmd = self.vim.vars.get('projectile#directory_command')
-        if not os.path.isdir(target['action__path']):
+        if not isdir(target['action__path']):
             return
-        destination = util.expand(target['action__path'])
+        destination = expand(target['action__path'])
         self.vim.call('execute', f'{user_cmd} {destination}')
 
-    # def action_cd(self, context):
-    #     """Change cwd to the project's root."""
-    #     target = context['targets'][0]
-    #     if not os.access(target['action__path']):
-    #         return
-    #     self.vim.command('lcd {}'.format(target['action__path']))
 
-    # def action_narrow(self, context):
-    #     """Traverse the path of a target."""
-    #     target = context['targets'][0]
-    #     context['sources_queue'].append([
-    #         {'name': 'file', 'args': []},
-    #         {'name': 'file', 'args': ['new']},
-    #     ])
-    #     context['path'] = target['action__path']
-
-    # def action_open(self, context):
-    #     """Open the target's action__path."""
-    #     for target in context['targets']:
-    #         path = target['action__path']
-    #         match_path = '^{0}$'.format(path)
-    #
-    #         if self.vim.call('bufwinnr', match_path) <= 0:
-    #             self.vim.call(
-    #                 'denite#util#execute_path', 'edit', path)
-    #         elif self.vim.call('bufwinnr',
-    #                            match_path) != self.vim.current.buffer:
-    #             self.vim.call(
-    #                 'denite#util#execute_path', 'buffer', path)
-
-    def _git_do(location, command):
-        """Run git commands from Python scripts.
-
-        Arguments
-        ---------
-        location : str
-            The target directory
-        command : str
-            The git command to execute
-
-        Returns
-        -------
-        list    : The utf-8 decoded search results, split by newline
-
-        """
-        # cmd = f"git -C {location} {command}"
-        cmd = f"git -C {location} status -s"
-        try:
-            p = subprocess.run(cmd,
-                               stdout=subprocess.PIPE,
-                               stderr=subprocess.STDOUT,
-                               shell=True)
-        except subprocess.CalledProcessError:
-            return []
-        return p.stdout.decode('utf-8').split('\n')
-
-    def _get_length(array):
-        """Get the max string length for an attribute in a collection."""
-        max_count = int(0)
-        for item in array:
-            cur_len = len(item)
-            if cur_len > max_count:
-                max_count = cur_len
-        return max_count
